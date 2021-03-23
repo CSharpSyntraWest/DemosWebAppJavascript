831e5b36c1b473dd5dc715f9fb49a32f4db5229d
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-23 15:47:50 +0100

    Demo leeftijdTekst function en innerHtml

--- Demo1Javascript/wwwroot/js/site.js
+++ Demo1Javascript/wwwroot/js/site.js
@@ -8,3 +8,29 @@ getal = 10;
 console.log(getal);
 const constanteWaarde = 'constante waarde';
 document.write('<h1>' + constanteWaarde + '</h1>');
+//berekening wordt aangeroepen in Demo10.html
+function berekening() {
+    let x = 1;
+    let y = 2;
+    alert(x + y);
+}
+
+//leeftijdTekst() wordt aangeroepen in Demo11.html
+function leeftijdTekst() {
+    //16;
+    let age = document.getElementById("txtAge").value
+    let commentaardiv = document.getElementById("commentaar");
+    if (age < 12) {
+        commentaardiv.innerHTML = "<h1>Te jong</h1>";
+        //document.write("Te jong");
+        //alert("Niet voor minderjarigen!");
+    }
+    else if (age < 18) {
+        commentaardiv.innerHTML = "<h1>Jong genoeg</h1>";
+        //document.write("Jong genoeg");
+    }
+    else {
+        commentaardiv.innerHTML = "<h1>Volwassen</h1>";
+        //document.write("volwassen");
+    }
+}
